89734c750220a3435c984d82e0576b38593fd82a
Nacho <nacho@Nachos-iMac.local>
2018-08-24 15:01:15 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatInputModule } from '@angular/material';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
